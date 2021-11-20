d553a0becdfa21948880255ba17bb9bdfd9b1603
Jean Maidana <jean0king@gmail.com>
2021-11-20 17:57:02 -0300

    Resposta via json

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,1 +1,19 @@
-const express = require('express')
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) =>{
+  res.send("<h1>Minha Lista de Tarefas :D</h1>");
+});
+
+app.get('/json',(req,res) => {
+  res.json({ // Resposta via json
+    title: "Tarefa x",
+    done: true
+
+  })
+});
+
+app.listen(3333, ()=>{
+  console.log("Server iniciado!")
+})
