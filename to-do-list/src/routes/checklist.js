870f4988fe9d5faa52fb64aea8ffac463bdaed6c
Jean Maidana <jean0king@gmail.com>
2021-11-21 18:48:52 -0300

    Add conceito de EJS

--- to-do-list/src/routes/checklist.js
+++ to-do-list/src/routes/checklist.js
@@ -2,29 +2,64 @@ const express = require('express')
 
 const router = express.Router();
 
-router.get('/',(req,res) =>{
-  console.log('Olá');
-  res.send();
+const Checklist = require('../model/checklist')
+
+
+
+router.get('/', async (req,res) =>{
+  try {
+    let checklist = await Checklist.find({});
+    res.status(200).json(checklist);
+  } catch (error){
+    res.status(500).json(error)
+  }
 });
 
-router.get('/:id',(req,res) =>{
+router.get('/:id', async (req,res) =>{
+  try {
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+    
+  }
   console.log(req.params.id);
   res.send(`ID: ${req.params.id}`);
 });
 
-router.post('/',(req,res) =>{
-  console.log(req.body);
+router.post('/',async (req,res) =>{
+  let {name} = req.body;
+  try{
+    let checklist = await Checklist.create({name});
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
+  console.log(name);
   res.status(200).send(req.body);
 });
 
-router.put('/:id',(req,res) =>{
+router.put('/:id',async (req,res) =>{
+  let { name } = req.body
+  try {
+    let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true} );
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+    
+  }
+
   console.log(req.params.id);
   res.send(`PUT ID: ${req.params.id}`);
 });
 
-router.delete('/:id',(req,res) =>{
-  console.log(req.params.id);
-  res.send(`DELETE ID: ${req.params.id}`);
+router.delete('/:id',async (req,res) =>{
+  try {
+    let checklist = await Checklist.findByIdAndRemove(req.params.id);
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
 
