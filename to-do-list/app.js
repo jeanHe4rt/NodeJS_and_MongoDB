9515ef8255feef2afd4034085070a4e7357be7ba
Jean Maidana <jean0king@gmail.com>
2021-11-20 23:33:47 -0300

    Create HTTP

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,29 +1,14 @@
 const express = require('express');
+const checklistRouter = require('./src/routes/checklist');
+
 
 const app = express();
 
 app.use(express.json()); // Ele verifica se no body da requisição tem json da requ.
 
-const log = (req, res, next) => {
-  console.log(req.body);
-  console.log(`Data: ${Date.now()}`);
-}
+app.use('/checklists',checklistRouter);
 
-app.use(log);
-
-app.get('/',(req,res) =>{
-  res.send("<h1>Minha Lista de Tarefas :D</h1>");
-});
-
-app.get('/json',(req,res) => {
-  console.log(req.body);
-  res.json({ // Resposta via json
-    title: "Tarefa x",
-    done: true
-
-  })
-});
 
 app.listen(3333, ()=>{
   console.log("Server iniciado!")
-})
+});
